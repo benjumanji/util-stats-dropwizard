4d1715be132770b1a985947b0bae3221b4bceb72
Ben Edwards <edwards.benj@gmail.com>
2015-02-07 15:23:29 +0000

    init repo

---
+++ build.sbt
@@ -0,0 +1,19 @@
+seq(bintraySettings:_*)
+
+organization := "io.artfuldoge"
+
+name := "util-stats-dropwizard"
+
+version := "0.1"
+
+description := "A stats reciever backed by a dropwizard metrics registry"
+
+licenses ++= Seq("MIT" -> url("http://opensource.org/licenses/MIT"))
+
+scalaVersion := "2.10.4"
+
+libraryDependencies ++= Seq(
+  "com.twitter" %% "util-stats" % "6.23.0",
+  "io.dropwizard.metrics" % "metrics-core" % "3.1.0"
+)
+
---
+++ src/main/scala/io/artfuldodge/util/stats/MetricStatsReceiver.scala
@@ -0,0 +1,55 @@
+package io.artfuldodge.util.stats
+
+import com.codahale.metrics.{
+  Counter => MetricsCounter,
+  Gauge => MetricsGauge,
+  Histogram => MetricsHistogram,
+  MetricRegistry
+}
+import com.twitter.finagle.stats.{
+  Counter,
+  Gauge,
+  Stat,
+  StatsReceiver
+}
+
+private[stats] class WrappedCounter(counter: MetricsCounter) extends Counter {
+  def incr(delta: Int) = counter.inc(delta)
+}
+
+private[stats] class WrappedHistogram(histogram: MetricsHistogram) extends Stat {
+  def add(value: Float) = histogram.update(value.toLong)
+}
+
+private[stats] class WrappedGauage(metrics: MetricRegistry, name: String) extends Gauge
+{
+  def remove() { metrics.remove(name) }
+}
+
+class MetricsStatsReciever(metrics: MetricRegistry) extends StatsReceiver {
+
+  private[this] def toDotted(name: Seq[String]): String = name.mkString(".")
+
+  val repr = this
+  private[this] val _metrics = metrics
+
+  def counter(name: String*): Counter = {
+    new WrappedCounter(metrics.counter(toDotted(name)))
+  }
+
+  def stat(name: String*): Stat = {
+    new WrappedHistogram(metrics.histogram(toDotted(name)))
+  }
+
+  def addGauge(name: String*)(f: => Float): Gauge = {
+    val dotted: String = toDotted(name)
+
+    metrics.register(dotted, new MetricsGauge[Float] {
+      def getValue(): Float = f
+    })
+
+    new WrappedGauage(metrics, dotted)
+  }
+}
+
+object MetricsStatsReciever extends MetricsStatsReciever(new MetricRegistry)
